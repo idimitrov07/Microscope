28bf15b145952896894705560a25e5ef3c7f0eb1
Ivan Dimitrov <idimitrov07@gmail.com>
2014-02-25 23:34:26 +0200

    Added data to the posts collection

---
+++ server/fixtures.js
@@ -0,0 +1,20 @@
+// if db is empty fill with 3 fixtures
+if (Posts.find().count() === 0) {
+	Posts.insert({
+		title: 'Introduction to Telescope',
+		author: 'Sacha Greif',
+		url: 'http://sachagreif.com/introducing-telescope/'
+	});
+
+	Posts.insert({
+		title: 'Meteor',
+		author: 'Tom Coleman',
+		url: 'http://meteor.com'
+	});
+
+	Posts.insert({
+		title: 'The Meteor Book',
+		author: 'Tom Coleman',
+		url: 'http://themeteorbook.com'
+	});
+}
